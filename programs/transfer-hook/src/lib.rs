066c0d245abf46d5a2de2d3c3ffbdbc202b09bee
Mohammed Alabd <55356198+MohammedAlabd@users.noreply.github.com>
2024-02-29 22:02:41 +0300

    add the solution code

--- programs/transfer-hook/src/lib.rs
+++ programs/transfer-hook/src/lib.rs
@@ -3,33 +3,162 @@ use anchor_spl::{ token_2022, token, associated_token::AssociatedToken, token_in
 use spl_transfer_hook_interface::instruction::{ ExecuteInstruction, TransferHookInstruction };
 use spl_tlv_account_resolution::{account::ExtraAccountMeta, seeds::Seed, state::ExtraAccountMetaList};
 
-declare_id!("YOUR PROGRAM ID HERE");
+declare_id!("5FYsLEZ2vjDHmrs2UAVfDozy45zyPec26pjYvvgMiWhX");
 
 #[program]
 pub mod transfer_hook {
-    use super::*;
+  use anchor_spl::token::mint_to;
 
-    pub fn initialize_extra_account_meta_list(
-        ctx: Context<InitializeExtraAccountMetaList>,
-    ) -> Result<()> {
-        Ok(())
-    }
+  use super::*;
 
-    pub fn transfer_hook(ctx: Context<TransferHook>, amount: u64) -> Result<()> {
-        Ok(())
-    }
+  pub fn initialize_extra_account_meta_list(ctx: Context<InitializeExtraAccountMetaList>) -> Result<()> {
+    // index 0-3 are the accounts required for token transfer (source, mint, destination, owner)
+    let account_metas = vec![
+      // index 4, Token 22 program
+      ExtraAccountMeta::new_with_pubkey(&token_2022::ID, false, false)?,
+      // index 5, Token program
+      ExtraAccountMeta::new_with_pubkey(&token::ID, false, false)?,
+      // index 6, associated token program
+      ExtraAccountMeta::new_with_pubkey(&anchor_spl::associated_token::ID, false, false)?,
+      // index 7, crumb mint
+      ExtraAccountMeta::new_with_pubkey(&ctx.accounts.crumb_mint.key(), false, true)?,
+      // index 8, mint authority
+      ExtraAccountMeta::new_with_seeds(
+        &[
+          Seed::Literal {
+            bytes: "mint-authority".as_bytes().to_vec(),
+          },
+        ],
+        false, // is_signer
+        true // is_writable
+      )?,
+      // index 9, ATA
+      ExtraAccountMeta::new_with_pubkey(&ctx.accounts.crumb_mint_ata.key(), false, true)?
+    ];
 
-    pub fn fallback<'info>(
-        program_id: &Pubkey,
-        accounts: &'info [AccountInfo<'info>],
-        data: &[u8],
-    ) -> Result<()> {
-        Ok(())
+    // calculate account size
+    let account_size = ExtraAccountMetaList::size_of(account_metas.len())? as u64;
+    // calculate minimum required lamports
+    let lamports = Rent::get()?.minimum_balance(account_size as usize);
+
+    let mint = ctx.accounts.mint.key();
+    let signer_seeds: &[&[&[u8]]] = &[&[b"extra-account-metas", &mint.as_ref(), &[ctx.bumps.extra_account_meta_list]]];
+
+    // create ExtraAccountMetaList account
+    create_account(
+      CpiContext::new(ctx.accounts.system_program.to_account_info(), CreateAccount {
+        from: ctx.accounts.payer.to_account_info(),
+        to: ctx.accounts.extra_account_meta_list.to_account_info(),
+      }).with_signer(signer_seeds),
+      lamports,
+      account_size,
+      ctx.program_id
+    )?;
+
+    // initialize ExtraAccountMetaList account with extra accounts
+    ExtraAccountMetaList::init::<ExecuteInstruction>(
+      &mut ctx.accounts.extra_account_meta_list.try_borrow_mut_data()?,
+      &account_metas
+    )?;
+
+    Ok(())
+  }
+
+  pub fn transfer_hook(ctx: Context<TransferHook>, _amount: u64) -> Result<()> {
+    let signer_seeds: &[&[&[u8]]] = &[&[b"mint-authority", &[ctx.bumps.mint_authority]]];
+   // mint a crumb token for each transaction
+    mint_to(
+      CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        token::MintTo {
+          mint: ctx.accounts.crumb_mint.to_account_info(),
+          to: ctx.accounts.crumb_mint_ata.to_account_info(),
+          authority: ctx.accounts.mint_authority.to_account_info(),
+        },
+        signer_seeds
+      ),
+      1
+    ).unwrap();
+
+    Ok(())
+  }
+
+  // fallback instruction handler as workaround to anchor instruction discriminator check
+  pub fn fallback<'info>(program_id: &Pubkey, accounts: &'info [AccountInfo<'info>], data: &[u8]) -> Result<()> {
+    let instruction = TransferHookInstruction::unpack(data)?;
+
+    // match instruction discriminator to transfer hook interface execute instruction
+    // token2022 program CPIs this instruction on token transfer
+    match instruction {
+      TransferHookInstruction::Execute { amount } => {
+        let amount_bytes = amount.to_le_bytes();
+
+        // invoke custom transfer hook instruction on our program
+        __private::__global::transfer_hook(program_id, accounts, &amount_bytes)
+      }
+      _ => {
+        return Err(ProgramError::InvalidInstructionData.into());
+      }
     }
+  }
 }
 
 #[derive(Accounts)]
-pub struct InitializeExtraAccountMetaList {}
+pub struct InitializeExtraAccountMetaList<'info> {
+  #[account(mut)]
+  payer: Signer<'info>,
 
+  /// CHECK: ExtraAccountMetaList Account, must use these seeds
+  #[account(
+        mut,
+        seeds = [b"extra-account-metas", mint.key().as_ref()], 
+        bump
+    )]
+  pub extra_account_meta_list: AccountInfo<'info>,
+  pub mint: InterfaceAccount<'info, Mint>,
+  pub token_program: Interface<'info, TokenInterface>,
+  pub associated_token_program: Program<'info, AssociatedToken>,
+  pub system_program: Program<'info, System>,
+  // #[account(init, payer = payer, space = 16)]
+  #[account(init, payer = payer, mint::decimals = 0, mint::authority = mint_authority)]
+  pub crumb_mint: InterfaceAccount<'info, Mint>,
+  /// CHECK: mint authority Account,
+  #[account(seeds = [b"mint-authority"], bump)]
+  pub mint_authority: UncheckedAccount<'info>,
+  /// CHECK: ATA,
+  pub crumb_mint_ata: UncheckedAccount<'info>,
+}
+
+// Order of accounts matters for this struct.
+// The first 4 accounts are the accounts required for token transfer (source, mint, destination, owner)
+// Remaining accounts are the extra accounts required from the ExtraAccountMetaList account
+// These accounts are provided via CPI to this program from the token2022 program
 #[derive(Accounts)]
-pub struct TransferHook {}
+pub struct TransferHook<'info> {
+  #[account(token::mint = mint, token::authority = owner)]
+  pub source_token: InterfaceAccount<'info, TokenAccount>,
+  pub mint: InterfaceAccount<'info, Mint>,
+  #[account(token::mint = mint)]
+  pub destination_token: InterfaceAccount<'info, TokenAccount>,
+  /// CHECK: source token account owner, can be SystemAccount or PDA owned by another program
+  pub owner: UncheckedAccount<'info>,
+
+  /// CHECK: ExtraAccountMetaList Account,
+  #[account(seeds = [b"extra-account-metas", mint.key().as_ref()], bump)]
+  pub extra_account_meta_list: UncheckedAccount<'info>,
+
+  pub token_extension_program: Interface<'info, TokenInterface>,
+
+  pub token_program: Interface<'info, TokenInterface>,
+
+  pub associated_token_program: Program<'info, AssociatedToken>,
+
+  pub crumb_mint: InterfaceAccount<'info, Mint>,
+
+  /// CHECK: mint authority Account,
+  #[account(seeds = [b"mint-authority"], bump)]
+  pub mint_authority: UncheckedAccount<'info>,
+
+  #[account(token::mint = crumb_mint)]
+  pub crumb_mint_ata: InterfaceAccount<'info, TokenAccount>,
+}
--- tests/anchor.ts
+++ tests/anchor.ts
@@ -34,15 +34,221 @@ describe('transfer-hook', () => {
 
   const connection = provider.connection;
 
+  // Generate keypair to use as address for the transfer-hook enabled mint
+  const mint = new Keypair();
+
+  const crumbMint = new Keypair();
+
+  // Sender token account address
+  const sourceTokenAccount = getAssociatedTokenAddressSync(
+    mint.publicKey,
+    wallet.publicKey,
+    false,
+    TOKEN_2022_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+  );
+
+  // Recipient token account address
+  const recipient = Keypair.generate();
+  console.log('Recipient:', recipient.publicKey.toBase58());
+  const destinationTokenAccount = getAssociatedTokenAddressSync(
+    mint.publicKey,
+    recipient.publicKey,
+    false,
+    TOKEN_2022_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+  );
+
+  // ExtraAccountMetaList address
+  // Store extra accounts required by the custom transfer hook instruction
+  const [extraAccountMetaListPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from('extra-account-metas'), mint.publicKey.toBuffer()],
+    program.programId,
+  );
+
+  // PDA from the transfer hook program to be used as the mint authority for the crumb mint
+  const [crumbMintAuthority] = PublicKey.findProgramAddressSync([Buffer.from('mint-authority')], program.programId);
+
+  // Associated token account for the crumb mint
+  const crumbMintATA = getAssociatedTokenAddressSync(crumbMint.publicKey, crumbMintAuthority, true);
+
   before('Airdrop SOL', async () => {
     await airdropSolIfNeeded(wallet.payer, provider.connection);
   });
 
-  it('Creates an NFT with Transfer Hook Extension and Metadata', async () => {});
+  it('Creates an NFT with Transfer Hook Extension and Metadata', async () => {
+    const metadata = await getMetadataObject({
+      connection,
+      imagePath: 'helpers/cool-cookie.png',
+      tokenName: 'Cool Cookie',
+      tokenSymbol: 'COOKIE',
+      tokenDescription: 'A cool cookie',
+      mintPublicKey: mint.publicKey,
+      additionalMetadata: [],
+      payer: wallet.payer,
+    });
 
-  it('Creates Token Accounts and Mint The NFT', async () => {});
+    // NFT Should have 0 decimals
+    const decimals = 0;
 
-  it('Initializes ExtraAccountMetaList Account and Creates the ATA for the Crumb Mint', async () => {});
+    const extensions = [ExtensionType.TransferHook, ExtensionType.MetadataPointer];
+    const mintLen = getMintLen(extensions);
+    const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+    const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
 
-  it('Transfers the NFT and the transfer hook mints a crumb token for each transfer', async () => {});
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: mint.publicKey,
+        space: mintLen,
+        lamports: lamports,
+        programId: TOKEN_2022_PROGRAM_ID,
+      }),
+      createInitializeMetadataPointerInstruction(
+        mint.publicKey,
+        wallet.publicKey,
+        mint.publicKey,
+        TOKEN_2022_PROGRAM_ID,
+      ),
+      createInitializeTransferHookInstruction(
+        mint.publicKey,
+        wallet.publicKey,
+        program.programId, // Transfer Hook Program ID
+        TOKEN_2022_PROGRAM_ID,
+      ),
+      createInitializeMintInstruction(mint.publicKey, decimals, wallet.publicKey, null, TOKEN_2022_PROGRAM_ID),
+      createInitializeInstruction({
+        programId: TOKEN_2022_PROGRAM_ID,
+        mint: mint.publicKey,
+        metadata: mint.publicKey,
+        name: metadata.name,
+        symbol: metadata.symbol,
+        uri: metadata.uri,
+        mintAuthority: wallet.publicKey,
+        updateAuthority: wallet.publicKey,
+      }),
+    );
+
+    const txSig = await sendAndConfirmTransaction(provider.connection, transaction, [wallet.payer, mint]);
+    console.log(
+      'Transaction Signature:',
+      `https://explorer.solana.com/tx/${txSig}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`,
+    );
+  });
+
+  // Create the two token accounts for the transfer-hook enabled mint
+  // Fund the sender token account with 100 tokens
+  it('Creates Token Accounts and Mint The NFT', async () => {
+    // 1 NFT
+    const amount = 1;
+
+    const transaction = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        wallet.publicKey,
+        sourceTokenAccount,
+        wallet.publicKey,
+        mint.publicKey,
+        TOKEN_2022_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+      ),
+      createAssociatedTokenAccountInstruction(
+        wallet.publicKey,
+        destinationTokenAccount,
+        recipient.publicKey,
+        mint.publicKey,
+        TOKEN_2022_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID,
+      ),
+      createMintToInstruction(mint.publicKey, sourceTokenAccount, wallet.publicKey, amount, [], TOKEN_2022_PROGRAM_ID),
+      createSetAuthorityInstruction(
+        mint.publicKey,
+        wallet.publicKey,
+        AuthorityType.MintTokens,
+        null,
+        [],
+        TOKEN_2022_PROGRAM_ID,
+      ),
+    );
+
+    const txSig = await sendAndConfirmTransaction(connection, transaction, [wallet.payer], { skipPreflight: true });
+
+    console.log(
+      'Transaction Signature:',
+      `https://explorer.solana.com/tx/${txSig}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`,
+    );
+  });
+
+  // Account to store extra accounts required by the transfer hook instruction
+  it('Initializes ExtraAccountMetaList Account and Creates the ATA for the Crumb Mint', async () => {
+    const initializeExtraAccountMetaListInstruction = await program.methods
+      .initializeExtraAccountMetaList()
+      .accounts({
+        mint: mint.publicKey,
+        extraAccountMetaList: extraAccountMetaListPDA,
+        crumbMint: crumbMint.publicKey,
+        crumbMintAta: crumbMintATA,
+      })
+      .instruction();
+
+    const transaction = new Transaction().add(
+      initializeExtraAccountMetaListInstruction,
+      createAssociatedTokenAccountInstruction(wallet.publicKey, crumbMintATA, crumbMintAuthority, crumbMint.publicKey),
+    );
+
+    const txSig = await sendAndConfirmTransaction(provider.connection, transaction, [wallet.payer, crumbMint], {
+      skipPreflight: true,
+      commitment: 'confirmed',
+    });
+    console.log(
+      'Transaction Signature:',
+      `https://explorer.solana.com/tx/${txSig}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`,
+    );
+  });
+
+  it('Transfers the NFT and the transfer hook mints a crumb token for each transfer', async () => {
+    const amount = 1;
+    const bigIntAmount = BigInt(amount);
+
+    // Standard token transfer instruction
+    const transferInstruction = await createTransferCheckedWithTransferHookInstruction(
+      connection,
+      sourceTokenAccount,
+      mint.publicKey,
+      destinationTokenAccount,
+      wallet.publicKey,
+      bigIntAmount,
+      0, // Decimals
+      [],
+      'confirmed',
+      TOKEN_2022_PROGRAM_ID,
+    );
+
+    const transferBackInstruction = await createTransferCheckedWithTransferHookInstruction(
+      connection,
+      destinationTokenAccount,
+      mint.publicKey,
+      sourceTokenAccount,
+      recipient.publicKey,
+      bigIntAmount,
+      0, // Decimals
+      [],
+      'confirmed',
+      TOKEN_2022_PROGRAM_ID,
+    );
+
+    const transaction = new Transaction().add(transferInstruction, transferBackInstruction);
+
+    const txSig = await sendAndConfirmTransaction(connection, transaction, [wallet.payer, recipient], {
+      skipPreflight: true,
+    });
+    console.log(
+      'Transfer Signature:',
+      `https://explorer.solana.com/tx/${txSig}?cluster=custom&customUrl=http%3A%2F%2Flocalhost%3A8899`,
+    );
+
+    const mintInfo = await getMint(connection, crumbMint.publicKey, 'processed');
+    console.log('Mint Info:', Number(mintInfo.supply));
+
+    expect(Number(mintInfo.supply)).to.equal(2);
+  });
 });
